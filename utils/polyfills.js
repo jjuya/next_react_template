7a633e9526a5bc00f9acf3d7d210da86c07d4fcf
jjuya <404_jjuya@kakao.com>
2022-02-17 09:34:17 +0900

    init: add polyfills

---
+++ utils/polyfills.js
@@ -0,0 +1,16 @@
+/* eslint no-extend-native: 0 */
+// core-js comes with Next.js. So, you can import it like below
+// import includes from 'core-js/stable/string/virtual/includes'
+// import repeat from 'core-js/stable/string/virtual/repeat'
+// import assign from 'core-js/stable/object/assign'
+
+// Add your polyfills
+import 'core-js/stable'
+import 'raf/polyfill'
+require('es6-promise').polyfill()
+// This files runs at the very beginning (even before React and Next.js core)
+console.log('Load your polyfills')
+
+// String.prototype.includes = includes
+// String.prototype.repeat = repeat
+// Object.assign = assign
